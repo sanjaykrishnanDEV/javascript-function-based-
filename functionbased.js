86c2ecbe51de37fce8513ce1208d73ac4f163692
Sanjay krishnan <er.sanjaykrishnan@gmail.com>
2023-08-02 20:07:53 +0530

    Create functionbased.js
    codespaces-function based javascript problems solutions

---
+++ functionbased.js
@@ -0,0 +1,13 @@
+
+Task 1 - a : https://codesandbox.io/s/upbeat-hypatia-l58s68?file=/src/index.js
+Task-2: https://codesandbox.io/s/angry-rgb-snjg4l?file=/src/index.js
+Task-3: https://codesandbox.io/s/withered-lake-g3cgs3?file=/src/index.js
+Task-4: https://codesandbox.io/s/agitated-khayyam-f6nxgr?file=/src/index.js
+Task-5: https://codesandbox.io/s/hidden-http-m2mc44?file=/src/index.js
+Task-6: https://codesandbox.io/s/romantic-brook-l4fw6j?file=/src/index.js:0-383
+Task-7:https://codesandbox.io/s/amazing-lena-fcqmk7?file=/src/index.js
+Task-8:  https://codesandbox.io/s/exciting-davinci-hqg38k?file=/src/index.js
+Task-9: https://codesandbox.io/s/zen-flower-375727?file=/src/index.js
+Task-10-a: https://codesandbox.io/s/nice-hill-fq94tf?file=/src/index.js
+Task-10-b: https://codesandbox.io/s/nice-hill-fq94tf?file=/src/index.js
+
